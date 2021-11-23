cedba42ee1dd5eb557476c2b2f28222aa2cec657
Alec Marcus <alec@alecmarc.us>
2021-11-23 03:40:01 -0500

    add authentication

--- src/App/index.js
+++ src/App/index.js
@@ -1,18 +1,48 @@
+import { useState, useRef } from "react";
 import styles from './app.module.scss';
 import { useSheetData } from '../hooks';
 import { useAttendees, useContributions } from "../hooks/queries";
-import { PeopleList, Main } from "../components";
+import { PeopleList, Main, Skype } from "../components";
 
 function App() {
   const sheetData = useSheetData();
   const attendees = useAttendees(sheetData);
   const contributions = useContributions(sheetData);
 
+  const [showLogin, setShowLogin] = useState(true);
+  const [showApp, setShowApp] = useState(false);
+  const appRef = useRef(null);
+  const loginScreenRef = useRef(null);
+
+  const authorize = ({ target: { value: password }}) => {
+    if (password === process.env.REACT_APP_PASSWORD) {
+      loginScreenRef.current.classList.add(styles.leave);
+      setShowApp(true);
+
+      setTimeout(() => {
+        setShowLogin(false);
+      }, 400);
+    }
+  }
+
   return (
-    <main className={styles.app}>
-      <Main />
-      <PeopleList attendees={attendees} contributions={contributions} loading={sheetData.loading} />
-    </main>
+    <>
+      {showApp && (
+        <main className={styles.app} ref={appRef}>
+          <Main />
+          <PeopleList attendees={attendees} contributions={contributions} loading={sheetData.loading} />
+        </main>
+      )}
+      {showLogin && (
+        <div className={styles.loginScreen} ref={loginScreenRef}>
+          <h1 className={styles.title}><span className={styles.noThanks}>Thanks<Skype className={styles.skype}/></span>giving</h1>
+          <p className={styles.year}>2021</p>
+          <form className={styles.passwordForm}>
+            <input placeholder="Password" type="password" name="password" id="password" onChange={authorize}  className={styles.passwordInput}/>
+          </form>
+        </div>
+      )}
+    </>
   );
 };
 
